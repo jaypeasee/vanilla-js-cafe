c6bed767cdb70cac0a96a6134dbfabc2d9452177
JP Carey <jpcarey4@gmail.com>
2021-03-16 19:30:43 -0600

    successfully post a new resy

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const allReservations = document.querySelector('.all-reservations')
+const reserveBtn = document.querySelector('.reserve-btn')
+const nameInput = document.querySelector('.name-input')
+const dateInput = document.querySelector('.date-input')
+const timeInput = document.querySelector('.time-input')
+const numberInput = document.querySelector('.number-input')
+
 
 let reservations = []
 
 window.addEventListener('load', pullReservations)
-
+reserveBtn.addEventListener('click', makeReservation)
 
 function pullReservations() {
   getReservations()
@@ -17,6 +23,7 @@ function getReservations() {
 }
 
 function displayReservations() {
+  allReservations.innerHTML = ""
   const reservationCards = reservations.map(resy => {
     return (
       `<section class="reservation-card" data-id=${resy.id}>
@@ -28,4 +35,35 @@ function displayReservations() {
     )
   })
   allReservations.insertAdjacentHTML('afterbegin', reservationCards)
+}
+
+function makeReservation(event) {
+  event.preventDefault()
+  const reservation = createReservation()
+  postReservation(reservation)
+    .then(data => reservations.push(data))
+    .then(displayReservations)
+}
+
+function createReservation() {
+  return (
+    {
+      id: Date.now(),
+      name: nameInput.value,
+      date: dateInput.value,
+      time: timeInput.value,
+      number: numberInput.value
+    }
+  )
+}
+
+function postReservation(reservation) {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reservation)
+  })
+    .then(response => response.json())
 }
